e8c1faeaed254335470bead578dfa24b9b9ec84e
jkim3118 <jkim3118@gatech.edu>
2018-04-29 01:03:21 -0400

    made it into a header file

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "image_processing.h"
 #include "extracter.h"
 #include "json/json.h"
+#include "search.h"
 #include <vector>
 
 //Need install these on linux
@@ -18,21 +19,8 @@
 using namespace std;
 int main() {
 
-    //This is for testing jsonparser
-//    string extractedString;
-//    string numResults;
-//    Json::Value root;
-//    Json::Reader reader;
-//
-//    ifstream file("../searchResult.JSON");
-//    file >> root;
-//
-//
-//    extractedString = getString(root);
-//    cout << extractedString;
-    
-//    numResults = getNumResults(root);
-//    cout << numResults << "\n";
+      string extractedString;
+      string numResults;
 
     //returns vector of strings, first string being the question, the following being the options
 //    vector<string> questions_answers = ocr_screenshot("../test_data/test1.png");
@@ -42,5 +30,20 @@ int main() {
 //        cout << questions_answers[i] << endl;
 //    }
 
+    // searching
+    std::vector<std::string> searches;
+    searches.emplace_back(createGoogleSearch("test", 4));
+    searches.emplace_back(createGoogleSearch("computer", 2));
+
+    // perform search
+    std::vector<std::string> outputs = getResults(searches);
+
+    // extract string from search result
+    extractedString = getString(outputs[0]);
+    cout << extractedString;
+
+    // extract number of results from search result
+    numResults = getNumResults(outputs[0]);
+    cout << numResults << "\n";
     return 0;
 }
